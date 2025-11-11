962ad0b6365113fdc977a5889b00ed871805be1e
wilberth <wilcr05@hotmail.com>
2025-11-11 14:12:04 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,79 @@
+from fastapi import FastAPI, Depends, HTTPException
+from datetime import date
+from pydantic import BaseModel
+
+from sqlalchemy import create_engine, Column, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
+app = FastAPI()
+
+DATABASE_URL = "sqlite:///tareas.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try: 
+        yield db
+    finally: 
+        db.close()
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+
+class TareaDB(Base):
+    __tablename__ = "tareas"
+
+    id = Column(Integer, primary_key=True, index=True)
+    titulo = Column(String)
+    descripcion = Column(String)
+    tiempo = Column(Date)
+
+Base.metadata.create_all(bind=engine)
+
+class UserCreate(BaseModel):
+    name: str
+    email: str
+
+@app.post("/users/")
+def create_user(user: UserCreate, db: Session=Depends(get_db)):
+    db_user = User(name=user.name, email= user.email)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+class Tarea(BaseModel):
+    titulo: str
+    descripcion: str
+    tiempo: date
+
+class Usuario(BaseModel):
+    correo: str
+    contraseña: str
+
+@app.post("/login")
+def login(usuario:Usuario):
+    if usuario.correo == "ejemplo@gmail.com" and usuario.contraseña == "1234":
+        return {"login"}
+    else:
+        return {"Incorrecto"}
+
+@app.get("/")
+def read_root():
+    return {"Hello world"}
+
+@app.get("/suma/{numero1}/{numero2}")
+def mensaje_2(numero1: int, numero2: int):
+    var = numero1 + numero2
+    return {var}
+
+
